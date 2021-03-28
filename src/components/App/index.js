5ad142f4165bbf69b936975b61b0896e5f44c2f9
Víctor M. Reyes <vm.reyesal@gmail.com>
2021-03-27 18:44:20 -0600

    feat: get categories

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,20 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { config } from "../../config";
 import { HeaderTop } from "../HeaderTop";
 import { SearchBar } from "../SearchBar";
 import "./styles.css";
 
-const { appName } = config;
+const { appName, products } = config;
 
 document.querySelector("title").innerText = `Inicio | ${appName}`;
 
 const App = () => {
-  return (
-    <React.Fragment>
-      <HeaderTop />
-      <SearchBar />
-    </React.Fragment>
-  );
+	return (
+		<React.Fragment>
+			<HeaderTop />
+			<SearchBar />
+		</React.Fragment>
+	);
 };
 
 export { App };
--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -3,56 +3,80 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles.css";
 import { config } from "../../config";
 import { BsSearch } from "react-icons/bs";
+import axios from "axios";
 
 import { FaRegHeart } from "react-icons/fa";
 import { RiShoppingBagLine } from "react-icons/ri";
 
-const { logo } = config;
-const categories = ["All Categories", "Electronic", "Fashion", "Gift"];
+const { logo, appName, products } = config;
+const unique = (value, index, item) => {
+	return item.indexOf(value) === index;
+};
 
 export const SearchBar = () => {
-  const [list, setList] = useState([]);
-  useEffect(() => {
-    let list = categories.map((e, i) => (
-      <option key={i} value={i}>
-        {e}
-      </option>
-    ));
-    setList(list);
-  }, []);
+	const [list, setList] = useState(["Todas las categorías"]);
+	const [categories, setCategories] = useState([]);
 
-  return (
-    <React.Fragment>
-      <div className="search-bar">
-        <div className="container">
-          <img src={logo} id="logo" className="img-fluid" />
-          <div className="input-group">
-            <input type="text" class="form-control" placeholder="Buscar" />
-            <select
-              className="px-3"
-              name="categories"
-              id="search-bar__categories"
-            >
-              {list}
-            </select>
-            <button
-              className="btn btn-outline-secondary"
-              type="button"
-              id="search"
-            >
-              <BsSearch />
-            </button>
-          </div>
-          <div className="whishlist-and-shopping-bag">
-            <a href="#">
-              <FaRegHeart className="whishlist-and-shopping-bag__icon" />
-            </a>
-            <a href="#">
-              <RiShoppingBagLine className="whishlist-and-shopping-bag__icon" />
-            </a>
-          </div>
-        </div>
-      </div>
-    </React.Fragment>
-  );
+	useEffect(() => {
+		let list = categories.map((e, i) => (
+			<option key={i} value={i}>
+				{e}
+			</option>
+		));
+		setList(list);
+	}, [categories]);
+
+	useEffect(() => {
+		axios
+			.get(products)
+			.then(r => {
+				let data = r.data;
+				let categoriesList = [];
+				data.map(c => {
+					c.category != undefined && categoriesList.push(c.category);
+				});
+
+				setCategories(categoriesList.filter(unique));
+			})
+			.catch(e => {
+				console.log(e);
+			});
+	}, []);
+	return (
+		<React.Fragment>
+			<div className="search-bar">
+				<div className="container">
+					<img src={logo} id="logo" className="img-fluid" />
+					<div className="input-group">
+						<input
+							type="text"
+							className="form-control"
+							placeholder="Buscar"
+						/>
+						<select
+							className="px-3"
+							name="categories"
+							id="search-bar__categories">
+							<option value="-1">Todas las categorías</option>
+							{list}
+						</select>
+						<button
+							className="btn btn-outline-secondary"
+							type="button"
+							id="search">
+							<BsSearch />
+						</button>
+					</div>
+					<div className="whishlist-and-shopping-bag">
+						<a href="#">
+							<FaRegHeart className="whishlist-and-shopping-bag__icon" />
+						</a>
+						<a href="#">
+							<RiShoppingBagLine className="whishlist-and-shopping-bag__icon" />
+						</a>
+					</div>
+				</div>
+			</div>
+		</React.Fragment>
+	);
 };
