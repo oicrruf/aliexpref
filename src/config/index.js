9e8f0f327e6d39e3c954c97938541f0e18e61fa5
Víctor M. Reyes <vm.reyesal@gmail.com>
2021-03-29 00:43:38 -0600

    feat: set session

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,8 @@
 export const config = {
-	appName: "AliExpref",
-	logo:
-		"https://devf-website.s3.amazonaws.com/static/assets/img/logo-devf-white.svg",
-	products: "https://ecomerce-master.herokuapp.com/api/v1/item",
-	register: "https://ecomerce-master.herokuapp.com/api/v1/signup",
+  appName: "AliExpref",
+  logo:
+    "https://devf-website.s3.amazonaws.com/static/assets/img/logo-devf-white.svg",
+  products: "https://ecomerce-master.herokuapp.com/api/v1/item",
+  register: "https://ecomerce-master.herokuapp.com/api/v1/signup",
+  login: "https://ecomerce-master.herokuapp.com/api/v1/login",
 };
