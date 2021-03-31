271e5731b64311de72b77a5a9b79be4cc535a174
Víctor M. Reyes <vm.reyesal@gmail.com>
2021-03-31 03:15:01 -0600

    feat: config github pages

--- src/components/organisms/HeaderTop/index.js
+++ src/components/organisms/HeaderTop/index.js
@@ -7,75 +7,93 @@ import { Dropdown } from "../../molecules";
 import "./styles.css";
 import axios from "axios";
 
-const { appName, me } = config;
+const { appName, me, domain } = config;
 
-const AccountButtons = (props) => {
-  const [user, setUser] = useState({});
-  useEffect(() => {
-    axios
-      .get(me, {
-        headers: {
-          "Content-Type": "application/json",
-          "X-Requested-With": "XMLHttpRequest",
-          Authorization: `JWT ${localStorage.getItem("@session")}`,
-        },
-      })
-      .then((r) => {
-        setUser(r.data.user);
-      })
-      .catch((e) => {
-        console.log(e);
-      });
-  }, []);
+const AccountButtons = props => {
+	const [user, setUser] = useState({});
+	useEffect(() => {
+		axios
+			.get(me, {
+				headers: {
+					"Content-Type": "application/json",
+					"X-Requested-With": "XMLHttpRequest",
+					Authorization: `JWT ${localStorage.getItem("@session")}`,
+				},
+			})
+			.then(r => {
+				setUser(r.data.user);
+			})
+			.catch(e => {
+				console.log(e);
+			});
+	}, []);
 
-  if (props.isAuthenticated) {
-    return (
-      <React.Fragment>
-        |
-        <Link to="/me">
-          ¡Bienvenido {`${user.first_name} ${user.last_name}`}!
-          <img src="/assets/img/avatar.png" height="16" className="ml-2" />
-        </Link>
-      </React.Fragment>
-    );
-  } else {
-    return (
-      <React.Fragment>
-        |<Link to="/login">Iniciar Sesión</Link>|
-        <Link to="/singup">Crear cuenta</Link>
-      </React.Fragment>
-    );
-  }
+	if (props.isAuthenticated) {
+		return (
+			<React.Fragment>
+				|
+				<Link to="/me">
+					¡Bienvenido {`${user.first_name} ${user.last_name}`}!
+					<img
+						src={
+							window.location.hostname != "oicrruf.github.io"
+								? "/assets/img/avatar.png"
+								: `${domain}/assets/img/avatar.png`
+						}
+						height="16"
+						className="ml-2"
+					/>
+				</Link>
+			</React.Fragment>
+		);
+	} else {
+		return (
+			<React.Fragment>
+				|<Link to="/login">Iniciar Sesión</Link>|
+				<Link to="/singup">Crear cuenta</Link>
+			</React.Fragment>
+		);
+	}
 };
 
-export const HeaderTop = (props) => {
-  const [countriesList, setCountriesList] = useState([]);
-  const [currencyList, setCurrencyList] = useState([]);
-  useEffect(() => {
-    let countries = [];
-    let currency = [];
-    country.map((c) => {
-      countries.push({ value: c.name, flag: c.flag });
-      currency.push({ value: c.currency, symbol: c.symbol });
-    });
-    setCountriesList(countries);
-    setCurrencyList(currency);
-  }, []);
-  return (
-    <React.Fragment>
-      <div className="navbar__header">
-        <div className="container">
-          <div className="navbar__header-international">
-            <Dropdown name={"Moneda"} type={"currency"} data={currencyList} />
-            <Dropdown name={"País"} type={"country"} data={countriesList} />
-          </div>
-          <div className="navbar__header-nav">
-            <a href="#">Bienvenido a {appName}</a>|
-            <a href="/contact">Contáctanos</a>
-            <AccountButtons isAuthenticated={props.isAuthenticated} />
-          </div>
-        </div>
-      </div>
-    </React.Fragment>
-  );
+export const HeaderTop = props => {
+	const [countriesList, setCountriesList] = useState([]);
+	const [currencyList, setCurrencyList] = useState([]);
+	useEffect(() => {
+		let countries = [];
+		let currency = [];
+		country.map(c => {
+			countries.push({ value: c.name, flag: c.flag });
+			currency.push({ value: c.currency, symbol: c.symbol });
+		});
+		setCountriesList(countries);
+		setCurrencyList(currency);
+	}, []);
+	return (
+		<React.Fragment>
+			<div className="navbar__header">
+				<div className="container">
+					<div className="navbar__header-international">
+						<Dropdown
+							name={"Moneda"}
+							type={"currency"}
+							data={currencyList}
+						/>
+						<Dropdown
+							name={"País"}
+							type={"country"}
+							data={countriesList}
+						/>
+					</div>
+					<div className="navbar__header-nav">
+						<a href="#">Bienvenido a {appName}</a>|
+						<a href="/contact">Contáctanos</a>
+						<AccountButtons
+							isAuthenticated={props.isAuthenticated}
+						/>
+					</div>
+				</div>
+			</div>
+		</React.Fragment>
+	);
 };
--- src/components/organisms/ProductDetail/index.js
+++ src/components/organisms/ProductDetail/index.js
@@ -6,94 +6,99 @@ import { auth } from "../../../utils";
 import { Rating } from "../../atoms/Rating";
 import "./styles.css";
 
-const { appName, single_product } = config;
-export const ProductDetail = (props) => {
-  const [product, setProduct] = useState({});
-  const [isAuthenticated, setIsAuthenticated] = useState();
-  useEffect(() => {
-    setIsAuthenticated(auth.isAuthenticated());
-    axios
-      .get(`${single_product + props.id}`)
-      .then((r) => {
-        r.status === 200 && setProduct(r.data);
-      })
-      .catch((e) => {
-        console.log(e);
-      });
-  }, []);
-  document.querySelector(
-    "title"
-  ).innerText = `${product.product_name} | ${appName}`;
-  return (
-    <React.Fragment>
-      <div className="border-bottom">
-        <div className="container">
-          <nav aria-label="breadcrumb">
-            <ol className="breadcrumb">
-              <li className="breadcrumb-item">
-                <Link to="/">Inicio</Link>
-              </li>
-              <li className="breadcrumb-item active" aria-current="page">
-                {product.product_name}
-              </li>
-            </ol>
-          </nav>
-        </div>
-      </div>
+const { appName, single_product, domain } = config;
+export const ProductDetail = props => {
+	const [product, setProduct] = useState({});
+	const [isAuthenticated, setIsAuthenticated] = useState();
+	useEffect(() => {
+		setIsAuthenticated(auth.isAuthenticated());
+		axios
+			.get(`${single_product + props.id}`)
+			.then(r => {
+				r.status === 200 && setProduct(r.data);
+			})
+			.catch(e => {
+				console.log(e);
+			});
+	}, []);
+	document.querySelector(
+		"title"
+	).innerText = `${product.product_name} | ${appName}`;
+	return (
+		<React.Fragment>
+			<div className="border-bottom">
+				<div className="container">
+					<nav aria-label="breadcrumb">
+						<ol className="breadcrumb">
+							<li className="breadcrumb-item">
+								<Link to="/">Inicio</Link>
+							</li>
+							<li
+								className="breadcrumb-item active"
+								aria-current="page">
+								{product.product_name}
+							</li>
+						</ol>
+					</nav>
+				</div>
+			</div>
 
-      <div className="container">
-        <div className="row product-detail">
-          <div className="col-lg-6">
-            <img
-              className="img-fluid"
-              src={
-                product.image && product.image.includes(".")
-                  ? product.image
-                  : "/assets/img/product.jpg"
-              }
-            />
-          </div>
-          <div className="col-lg-6">
-            <h1>{product.product_name}</h1>
-            <Rating />
-            <p>{product.description}</p>
-            <hr />
-            <div className="stock-and-price">
-              <div className="stock-and-price__price">
-                <span>Desde:</span>
-                <h4>{`$${product.price}.00`}</h4>
-              </div>
-              <div className="stock-and-price__stock">
-                <span>
-                  Disponibilidad:
-                  {product.isActive ? " En stock" : " Agotado"}
-                </span>
-              </div>
-            </div>
-            {isAuthenticated ? (
-              <Link
-                to="/"
-                type="submit"
-                className="btn btn-primary btn-product 
-							mt-4"
-              >
-                {" "}
-                Añadir al carrito
-              </Link>
-            ) : (
-              <Link
-                to="/login"
-                type="submit"
-                className="btn btn-primary btn-product 
-							mt-4"
-              >
-                {" "}
-                Iniciar sesión
-              </Link>
-            )}
-          </div>
-        </div>
-      </div>
-    </React.Fragment>
-  );
+			<div className="container">
+				<div className="row product-detail">
+					<div className="col-lg-6">
+						<img
+							className="img-fluid"
+							src={
+								product.image && product.image.includes(".")
+									? product.image
+									: window.location.hostname !=
+									  "oicrruf.github.io"
+									? "/assets/img/product.jpg"
+									: `${domain}/assets/img/product.jpg`
+							}
+						/>
+					</div>
+					<div className="col-lg-6">
+						<h1>{product.product_name}</h1>
+						<Rating />
+						<p>{product.description}</p>
+						<hr />
+						<div className="stock-and-price">
+							<div className="stock-and-price__price">
+								<span>Desde:</span>
+								<h4>{`$${product.price}.00`}</h4>
+							</div>
+							<div className="stock-and-price__stock">
+								<span>
+									Disponibilidad:
+									{product.isActive
+										? " En stock"
+										: " Agotado"}
+								</span>
+							</div>
+						</div>
+						{isAuthenticated ? (
+							<Link
+								to="/"
+								type="submit"
+								className="btn btn-primary btn-product 
+							mt-4">
+								{" "}
+								Añadir al carrito
+							</Link>
+						) : (
+							<Link
+								to="/login"
+								type="submit"
+								className="btn btn-primary btn-product 
+							mt-4">
+								{" "}
+								Iniciar sesión
+							</Link>
+						)}
+					</div>
+				</div>
+			</div>
+		</React.Fragment>
+	);
 };
