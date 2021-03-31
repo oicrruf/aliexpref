6e32a4e58a65bf16073f15589e6f0354f52fc7b9
Víctor M. Reyes <vm.reyesal@gmail.com>
2021-03-31 12:27:18 -0600

    chore: responsive for small screens

--- src/components/organisms/Footer/index.js
+++ src/components/organisms/Footer/index.js
@@ -9,118 +9,120 @@ import axios from "axios";
 const { appName, products, domain } = config;
 
 export const Footer = () => {
-  const [list, setList] = useState(["Todas las categorías"]);
-  const [categories, setCategories] = useState([]);
+	const [list, setList] = useState(["Todas las categorías"]);
+	const [categories, setCategories] = useState([]);
 
-  useEffect(() => {
-    let list = categories.map((e, i) => (
-      <li key={i}>
-        <a href={`#${e.toLowerCase()}`}>{e}</a>
-      </li>
-    ));
-    setList(list);
-  }, [categories]);
+	useEffect(() => {
+		let list = categories.map((e, i) => (
+			<li key={i}>
+				<a href={`#${e.toLowerCase()}`}>{e}</a>
+			</li>
+		));
+		setList(list);
+	}, [categories]);
 
-  useEffect(() => {
-    axios
-      .get(products)
-      .then((r) => {
-        let data = r.data;
-        let categoriesList = [];
-        data.map((c) => {
-          c.category !== undefined && categoriesList.push(c.category);
-        });
+	useEffect(() => {
+		axios
+			.get(products)
+			.then(r => {
+				let data = r.data;
+				let categoriesList = [];
+				data.map(c => {
+					c.category !== undefined && categoriesList.push(c.category);
+				});
 
-        setCategories(categoriesList.filter(unique).sort());
-      })
-      .catch((e) => {
-        console.log(e);
-      });
-  }, []);
-  return (
-    <React.Fragment>
-      <div className="newsletter-block">
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-3">
-              <h4>Suscríbete al boletín</h4>
-            </div>
-            <div className="col-lg-5">
-              <p>
-                Obtenga toda la información más reciente sobre eventos, ventas y
-                ofertas.
-              </p>
-              <h5>Reciba un cupón de $ 10 para la primera compra.</h5>
-            </div>
-            <div className="col-lg-4">
-              <div className="input-group">
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Correo electrónico"
-                />
-                <button
-                  className="btn btn-outline-secondary"
-                  type="button"
-                  id="search"
-                >
-                  Suscribirse
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <footer>
-        <div className="container">
-          <div className="footer-content row">
-            <div className="col-lg-6">
-              <Link to="/">
-                <img
-                  src={
-                    window.location.hostname != "oicrruf.github.io"
-                      ? "/assets/img/logo-devf-black.svg"
-                      : `${domain}/assets/img/logo-devf-black.svg`
-                  }
-                  className="img-fluid w-25"
-                />
-              </Link>
-              <div className="contact-info">
-                <div className="info-detail">
-                  <span>Llámanos ahora:</span>
-                  <h6>+503 7859 0168</h6>
-                </div>
-                <div className="info-detail">
-                  <span>Correo electrónico</span>
-                  <h6>vm.reyesal@gmail.com</h6>
-                </div>
-                <div className="info-detail info-follow">
-                  <span>Síguenos</span>
-                  <h6>
-                    <FaFacebookF />
-                    <FaTwitter />
-                    <FaLinkedin />
-                  </h6>
-                </div>
-              </div>
-            </div>
-            <div className="col-lg-6">
-              <div className="categories-info">
-                <h6>Categorias</h6>
-                <ul className="categories-list">{list}</ul>
-              </div>
-            </div>
-          </div>
-          <hr />
-          <div className="footer-bottom">
-            <p>
-              © {appName}. {new Date().getFullYear()}. Reservados todos los
-              derechos
-            </p>
-            <img src="https://www.portotheme.com/magento2/porto/pub/media/wysiwyg/smartwave/porto/homepage/03/content/payment-icon.png" />
-          </div>
-        </div>
-      </footer>
-    </React.Fragment>
-  );
+				setCategories(categoriesList.filter(unique).sort());
+			})
+			.catch(e => {
+				console.log(e);
+			});
+	}, []);
+	return (
+		<React.Fragment>
+			<div className="newsletter-block">
+				<div className="container">
+					<div className="row">
+						<div className="col-lg-3">
+							<h4>Suscríbete al boletín</h4>
+						</div>
+						<div className="col-lg-5">
+							<p>
+								Obtenga toda la información más reciente sobre
+								eventos, ventas y ofertas.
+							</p>
+							<h5>
+								Reciba un cupón de $ 10 para la primera compra.
+							</h5>
+						</div>
+						<div className="col-lg-4">
+							<div className="input-group">
+								<input
+									type="text"
+									className="form-control"
+									placeholder="Correo electrónico"
+								/>
+								<button
+									className="btn btn-outline-secondary"
+									type="button"
+									id="search">
+									Suscribirse
+								</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<footer>
+				<div className="container">
+					<div className="footer-content row">
+						<div className="col-lg-6">
+							<Link to="/">
+								<img
+									src={
+										window.location.hostname !=
+										"oicrruf.github.io"
+											? "/assets/img/logo-devf-black.svg"
+											: `${domain}/assets/img/logo-devf-black.svg`
+									}
+									className="img-fluid"
+								/>
+							</Link>
+							<div className="contact-info">
+								<div className="info-detail">
+									<span>Llámanos ahora:</span>
+									<h6>+503 7859 0168</h6>
+								</div>
+								<div className="info-detail">
+									<span>Correo electrónico</span>
+									<h6>vm.reyesal@gmail.com</h6>
+								</div>
+								<div className="info-detail info-follow">
+									<span>Síguenos</span>
+									<h6>
+										<FaFacebookF />
+										<FaTwitter />
+										<FaLinkedin />
+									</h6>
+								</div>
+							</div>
+						</div>
+						<div className="col-lg-6">
+							<div className="categories-info">
+								<h6>Categorias</h6>
+								<ul className="categories-list">{list}</ul>
+							</div>
+						</div>
+					</div>
+					<hr />
+					<div className="footer-bottom">
+						<p>
+							© {appName}. {new Date().getFullYear()}. Reservados
+							todos los derechos
+						</p>
+						<img src="https://www.portotheme.com/magento2/porto/pub/media/wysiwyg/smartwave/porto/homepage/03/content/payment-icon.png" />
+					</div>
+				</div>
+			</footer>
+		</React.Fragment>
+	);
 };
